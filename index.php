28530cc7d062770fddc3b68a9269d9db87d5232d
wardDward <edward.taligatos03@gmail.com>
2025-06-10 19:07:38 +0800

    grid layout

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php include('./includes/head.php'); ?>
+
 <nav class="landing-nav navbar">
-    <img src="./assets/images/logo.png" class="logo" alt="">
+    <img src="./assets/images/logo.png" class="logo" alt="Logo">
     <div>
         <a href="#" class="guest-btn">Sign In</a>
         <a href="#" class="guest-btn">Sign Up</a>
     </div>
 </nav>
+
 <section class="landing-container">
-    <img src="./assets/images/nike1.png" alt="">
+    <img src="./assets/images/nike1.png" alt="Product image">
     <div>
         <h2>Lorem ipsum dolor sit amet.</h2>
         <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum magni officiis id, dicta ab ratione? Numquam aperiam illum corrupti nihil.</p>
